58e53ae36346d5f30ffac1ab1001674b0aabb608
maldevel <maldevel@mail.com>
2016-10-29 14:35:50 +0300

    Bandit Level 16 rewritten using pwntools

--- Bandit/Level16.py
+++ Bandit/Level16.py
@@ -1,12 +1,11 @@
-import paramiko
+from pwn import *
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('bandit.labs.overthewire.org', username='bandit16', password='cluFn7wTiGryunymYOu4RcffSxQluehd')
-stdin, stdout, stderr = ssh.exec_command("cat /etc/bandit_pass/bandit16 | openssl s_client -connect 127.0.0.1:31790 -quiet | sed -e 's/Correct!//' | sed '/^\s*$/d'")
-lines = stdout.readlines()
-key = ''
-for line in lines:
-        if line.strip():
-		key = key + line
-print key
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit16'
+password = 'cluFn7wTiGryunymYOu4RcffSxQluehd'
+cmd = "cat /etc/bandit_pass/bandit16 | openssl s_client -connect 127.0.0.1:31790 -quiet | sed -e 's/Correct!//' | sed '/^\s*$/d'"
+
+s =  ssh(host=hostname, user=username, password=password)
+ex = s.run(cmd)
+print ex.recvall()
+
