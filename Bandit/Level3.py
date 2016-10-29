0253920d0ef0f26d496ccc75601de0206e0f9def
maldevel <maldevel@mail.com>
2016-10-29 13:59:37 +0300

    Bandit Level 3 rewritten using pwntools

--- Bandit/Level3.py
+++ Bandit/Level3.py
@@ -1,9 +1,10 @@
-import paramiko
+from pwn import *
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('bandit.labs.overthewire.org', username='bandit2', password='CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9')
-stdin, stdout, stderr = ssh.exec_command('cat "spaces in this filename"')
-lines = stdout.readlines()
-for line in lines:
-        print line
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit2'
+password = 'CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9'
+
+s =  ssh(host=hostname, user=username, password=password)
+cmd = s.run('cat "spaces in this filename"')
+print cmd.recvall()
+
