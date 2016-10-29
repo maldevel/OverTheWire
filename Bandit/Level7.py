df6c1172aee9a6d69530470c75e968e6a8d9da14
maldevel <maldevel@mail.com>
2016-10-29 14:29:38 +0300

    Bandit Level 7 rewritten using pwntools

--- Bandit/Level7.py
+++ Bandit/Level7.py
@@ -1,10 +1,11 @@
-import paramiko
+from pwn import *
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('bandit.labs.overthewire.org', username='bandit6', password='DXjZPULLxYr17uwoI01bNLQbtFemEgo7')
-stdin, stdout, stderr = ssh.exec_command('find / -type f -user bandit7 -group bandit6 -size 33c 2> /dev/null | xargs cat')
-lines = stdout.readlines()
-for line in lines:
-        if line.strip():
-		print line
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit6'
+password = 'DXjZPULLxYr17uwoI01bNLQbtFemEgo7'
+cmd = 'find / -type f -user bandit7 -group bandit6 -size 33c 2> /dev/null | xargs cat'
+
+s =  ssh(host=hostname, user=username, password=password)
+ex = s.run(cmd)
+print ex.recvall()
+
