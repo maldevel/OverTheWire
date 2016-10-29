25c96b812ba2d2b801e4166195727f61630b09a6
maldevel <maldevel@mail.com>
2016-10-29 14:01:52 +0300

    Bandit Level 5 rewritten using pwntools

--- Bandit/Level5.py
+++ Bandit/Level5.py
@@ -1,9 +1,10 @@
-import paramiko
+from pwn import *
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('bandit.labs.overthewire.org', username='bandit4', password='pIwrPrtPN36QITSp3EQaw936yaFoFgAB')
-stdin, stdout, stderr = ssh.exec_command('cat < inhere/-file07')
-lines = stdout.readlines()
-for line in lines:
-        print line
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit4'
+password = 'pIwrPrtPN36QITSp3EQaw936yaFoFgAB'
+
+s =  ssh(host=hostname, user=username, password=password)
+cmd = s.run('cat < inhere/-file07')
+print cmd.recvall()
+
